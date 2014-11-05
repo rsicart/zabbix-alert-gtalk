1c0368776ad78fc761ef5a4b2e83ec75dc54938d
Roger Sicart <roger@adserverpub.com>
2014-11-05 16:59:27 +0100

    Handle message replies.

--- gtalk3.py
+++ gtalk3.py
@@ -4,6 +4,9 @@ import logging
 import sys, getopt
 
 import sleekxmpp
+import urllib.request
+import time
+import re
 
 class Gtalk(sleekxmpp.ClientXMPP):
 
@@ -24,6 +27,7 @@ class Gtalk(sleekxmpp.ClientXMPP):
 
 		self.argv = argv
 		self.gtalk = settings.gtalk
+		self.reply = settings.reply
 		self.validateArgs(self.argv)
 
 		# build alert
@@ -38,6 +42,11 @@ class Gtalk(sleekxmpp.ClientXMPP):
 		self.recipient = self.alert['recipient']
 		self.msg = "{}\n{}".format(self.alert['subject'], self.alert['message'])
 		self.add_event_handler("session_start", self.start, threaded=False)
+		self.add_event_handler("logout", self.logout, threaded=True, disposable=True)
+
+		# handle responses
+		if self.reply['manage']:
+			self.add_event_handler("message", self.message)
 
 
 	def usage(self):
@@ -64,6 +73,13 @@ class Gtalk(sleekxmpp.ClientXMPP):
 				self.usage()
 				sys.exit(4)
 
+		# check valid reply values
+		for key, value in self.reply.items():
+			if value is None or len(str(value)) == 0:
+				print("Invalid reply settings.")
+				self.usage()
+				sys.exit(4)
+
 
 	def start(self, event):
 
@@ -75,10 +91,42 @@ class Gtalk(sleekxmpp.ClientXMPP):
 						  mbody=self.msg,
 						  mtype='chat')
 
-		# Using wait=True ensures that the send queue will be
-		# emptied before ending the session.
+		# trigger logout countdown
+		self.event('logout')
+
+
+	def logout(self, data):
+		''' Disconnect after the specified number of seconds.
+		'''
+		time.sleep(self.reply['timeout'])
 		self.disconnect(wait=True)
 
+
+	def message(self, msg):
+		if msg['type'] in ('chat', 'normal'):
+			params = {
+				'msisdn': self.getSender(msg['from']),
+				'text': msg['body'],
+			}
+			response = self.callUrl(params)
+
+
+	def getSender(self, jid):
+		''' Returns the sender address of a sleekxmpp JID object.
+			@params JID object
+			@return string email
+		'''
+		return '{}@{}'.format(jid.username, jid.domain)
+
+
+	def callUrl(self, params):
+		''' Makes an http request with params as querystring.
+			@params dict of get parameters
+			@return string
+		'''
+		querystring = urllib.parse.urlencode(params)
+		data = urllib.request.urlopen('{}?{}'.format(self.reply['callbackUrl'], querystring)).read()
+		return data
 
 
 if __name__ == "__main__":
@@ -87,16 +135,11 @@ if __name__ == "__main__":
 	xmpp.register_plugin('xep_0030') # Service Discovery
 	xmpp.register_plugin('xep_0199') # XMPP Ping
 
-	# If you are working with an OpenFire server, you may need
-	# to adjust the SSL version used:
-	# xmpp.ssl_version = ssl.PROTOCOL_SSLv3
-
-	# If you want to verify the SSL certificates offered by a server:
-	# xmpp.ca_certs = "path/to/ca/cert"
-
 	# Connect to the XMPP server and start processing XMPP stanzas.
 	if xmpp.connect((xmpp.gtalk['server'], xmpp.gtalk['port'])):
-		xmpp.process(threaded=False)
+		xmpp.process(block=False)
 		print("Done")
+		sys.exit(0)
 	else:
 		print("Unable to connect.")
+		sys.exit(1)
