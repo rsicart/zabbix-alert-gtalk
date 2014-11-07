f59c0c608e19562256407646515c753029bf78c3
Roger Sicart <roger@adserverpub.com>
2014-11-07 16:56:04 +0100

    Update settings.example with new parameters.

--- settings.example.py
+++ settings.example.py
@@ -6,3 +6,9 @@ gtalk = {
     'username':'',
     'passwd':"",
 }
+
+reply = {
+    'manage': {{ vault.gtalk.replies.manage }},
+    'callbackUrl': "{{ vault.gtalk.replies.callbackUrl }}",
+    'timeout': {{ vault.gtalk.replies.timeout }},
+}
